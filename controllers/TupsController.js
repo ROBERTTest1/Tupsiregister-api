fd3c8f8b9552461605e44242b80768384ec6f545
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-12-01 17:40:36 +0200

    Get TUPS by api 01.12

--- controllers/TupsController.js
+++ controllers/TupsController.js
@@ -5,8 +5,32 @@ exports.getAll = async (req, res) => {
   const tups = await db.tups.findAll();
   console.log("getAll: " + tups);
   res.status(200).send(
-    tups.map(({ TupsID, Name }) => {
-      return { TupsID, Name };
+    tups.map(({ TupsID, Name, Brand }) => {
+      return { TupsID, Name, Brand };
     })
   );
+
+exports.GetByID =
+async (req, res) => {
+  const film = await getTups(req, res);
+  if (!tups) {return res.status(404).send({error: 'Tups not found'})}
+  return res.status(200).send(tups)
+}
+
+const getTups = 
+async (req, res) => {
+  const idNumber =req.params.TupsID;
+  console.log(idNumber)
+  //if(isNaN(idNumber)) {
+    //res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //return null;
+  //}
+  const tups = await db.tups.findByPk(idNumber)
+  if(!tups) {
+    res.status(404).send({Error: `Tups with this id was not found ${idNumber}`})
+    return null;
+  }
+  return tups;
+}
+
 };
--- routes/trRoutes.js
+++ routes/trRoutes.js
@@ -3,4 +3,6 @@ const TupsController = require("../controllers/TupsController")
 module.exports = (app) => {
     app.route("/tups")
     .get(TupsController.getAll)
+    app.route("/tups/:TupsId")
+    .get(TupsController.GetByID)
 }
