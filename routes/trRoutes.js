9060233496fd006fb7a6335e45c0462e6710b231
ROBERTTest1 <robi.tarlap123@gmail.com>
2025-12-02 15:00:37 +0200

    Small update

--- routes/trRoutes.js
+++ routes/trRoutes.js
@@ -1,6 +1,6 @@
 const TupsController = require("../controllers/TupsController");
 
 module.exports = (app) => {
-  app.route("/tups").get(TupsController.getAll);
+  app.route("/tups").get(TupsController.getAll).get(TupsController.create);
   app.route("/tups/:TupsId").get(TupsController.getByID);
 };
