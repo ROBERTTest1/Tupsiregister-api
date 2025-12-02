2d8ef4cf1fa73d12bc83f6b4982a0c193105bcdf
ROBERTTest1 <robi.tarlap123@gmail.com>
2025-12-02 12:47:14 +0200

    #436198 As an app developer I want to get a list of all <Tups> to show in my app.

--- controllers/TupsController.js
+++ controllers/TupsController.js
@@ -1,2 +1,12 @@
 const { db } = require("../db");
 const Utilities = require("./Utilities");
+
+exports.getAll = async (req, res) => {
+  const tups = await db.tups.findAll();
+  console.log("getAll: " + tups);
+  res.status(200).send(
+    tups.map(({ TupsID, Name, Brand }) => {
+      return { TupsID, Name, Brand };
+    })
+  );
+};
