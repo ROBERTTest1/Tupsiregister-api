32980ccb0551d98dd364fb9dcc5b53178316cf18
ROBERTTest1 <robi.tarlap123@gmail.com>
2025-12-02 15:25:25 +0200

    fix route

--- routes/trRoutes.js
+++ routes/trRoutes.js
@@ -1,6 +1,6 @@
 const TupsController = require("../controllers/TupsController");
 
 module.exports = (app) => {
-  app.route("/tups").get(TupsController.getAll).get(TupsController.create);
+  app.route("/tups").get(TupsController.getAll).post(TupsController.create);
   app.route("/tups/:TupsId").get(TupsController.getByID);
 };
