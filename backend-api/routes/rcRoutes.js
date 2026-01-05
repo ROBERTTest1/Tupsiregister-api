abce6bbb67c7bf0f43f0487e9d76802525dc712b
ROBERTTest1 <robi.tarlap123@gmail.com>
2026-01-05 16:59:06 +0200

    Small fix

--- backend-api/routes/rcRoutes.js
+++ backend-api/routes/rcRoutes.js
@@ -1,9 +1,12 @@
 const WorkerController = require("../controllers/WorkerController");
 
 module.exports = (app) => {
-  app.route("/tups").get(WorkerController.getAll).post(WorkerController.create);
   app
-    .route("/tups/:WorkerID")
+    .route("/worker")
+    .get(WorkerController.getAll)
+    .post(WorkerController.create);
+  app
+    .route("/worker/:WorkerID")
     .get(WorkerController.getByID)
     .delete(WorkerController.deleteByID)
     .put(WorkerController.modifyById);
