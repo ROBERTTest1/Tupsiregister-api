69a76aebda99efed5ae9447a311b556c847645a2
ROBERTTest1 <robi.tarlap123@gmail.com>
2026-01-12 17:59:23 +0200

    push from frontend branch

--- backend-api/routes/rcRoutes.js
+++ backend-api/routes/rcRoutes.js
@@ -1,13 +1,37 @@
 const WorkerController = require("../controllers/WorkerController");
+const ShiftController = require("../controllers/ShiftController");
+const ScheduleController = require("../controllers/ScheduleController");
 
 module.exports = (app) => {
+  // ------------------ WORKER ROUTES ------------------
   app
     .route("/worker")
     .get(WorkerController.getAll)
     .post(WorkerController.create);
+
   app
     .route("/worker/:WorkerID")
     .get(WorkerController.getByID)
     .delete(WorkerController.deleteByID)
     .put(WorkerController.modifyById);
+
+  // ------------------ SHIFT ROUTES ------------------
+  app.route("/shift").get(ShiftController.getAll).post(ShiftController.create);
+
+  app
+    .route("/shift/:ShiftID")
+    .get(ShiftController.getByID)
+    .delete(ShiftController.deleteByID);
+
+  // ------------------ SCHEDULE ROUTES ------------------
+  app
+    .route("/schedule")
+    .get(ScheduleController.getAll)
+    .post(ScheduleController.create);
+
+  app
+    .route("/schedule/:ScheduleID")
+    .get(ScheduleController.getByID)
+    .delete(ScheduleController.deleteByID)
+    .put(ScheduleController.modifyById);
 };
